baabb0810420f9d3ef36a5428fd7dce4c1931ea1
german5486 <a.germanov@genesys.com>
2022-07-11 02:32:18 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import i18next from 'i18next';
+import { WithTranslation, withTranslation } from 'react-i18next';
 
-function App() {
+function App({t}:WithTranslation ) {
+  const setLanguage = (language: string) => {
+    i18next.changeLanguage(language);
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={() => setLanguage("en")}>ENGLISH</button>
+        <button onClick={() => setLanguage("ru")}>ru</button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        {t('High')}
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { setupTranslation } from './i18n/i18n';
+import { withTranslation } from 'react-i18next';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+setupTranslation().then(() => {
+    const AppContainer = withTranslation()(App);
+    ReactDOM.render(
+      <React.Suspense fallback={'Loading'}>
+        {/* <Auth0Provider
+          domain="dev-ufz05wjq.us.auth0.com"
+          clientId="NmNtaXKZAzyKjKVissbYbifjCfGESxwg"
+          redirectUri={window.location.origin}
+        > */}
+          <AppContainer />
+        {/* </Auth0Provider> */}
+      </React.Suspense>,
+      document.getElementById("root")
+    );
+  });
 reportWebVitals();
